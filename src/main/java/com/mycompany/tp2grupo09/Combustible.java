6a82ca9e8a7cf5cc07556b9d3dec1f32536eb748
MaxiLedesma93 <maximilianoledesma93@gmail.com>
2022-09-15 20:54:33 -0300

    clase combustible

---
+++ src/main/java/com/mycompany/tp2grupo09/Combustible.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tp2grupo09;
+
+/**
+ *
+ * @author maxim
+ */
+public class Combustible {
+    private String tipo;
+    private double precio;
+
+    public Combustible(String tipo, double precio) {
+        this.tipo = tipo;
+        this.precio = precio;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    
+}
